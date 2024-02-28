610f5bfeb672f7f55ab11571ece8f0df161ca4ab
Karol Klimończyk <karol.klimonczyk@gmail.com>
2024-02-28 18:03:53 +0100

    Add files via upload

---
+++ BSF.cpp
@@ -0,0 +1,116 @@
+﻿#include <iostream>
+#include <fstream>
+#include <list>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <random>
+
+using namespace std;
+// Data structure to store a graph edge
+struct Edge
+{
+	int src, dest, value;
+};
+// A class to represent a graph object
+class Graph
+{
+public:
+	// a vector of vectors to represent an adjacency list
+	vector<vector<int>> adjList;
+	vector<int> numbers;
+	// Graph Constructor
+	Graph(vector<Edge> const& edges, int n)
+	{
+		// resize the vector to hold `n` elements of type `vector<int>`
+		adjList.resize(n);
+		// add edges to the undirected graph
+		for (auto& edge : edges)
+		{
+			adjList[edge.src].push_back(edge.dest);
+			adjList[edge.dest].push_back(edge.src);
+			numbers.push_back(edge.value);
+		}
+		// randomizing values ​​and showing odd numbers and the vertex number
+		cout << "Random numbers: ";
+		for (int i = 0; i < numbers.size(); i++) {
+			cout << numbers[i] << " ";
+		}
+		cout << endl;
+		cout << "Odd numbers:" << "\t" << "->" << "\t" << "no. edge:" << endl;
+		for (int i = 0; i < numbers.size(); i++) {
+			if (numbers[i] % 2 != 0) {
+				cout << numbers[i] << " \t \t \t \t" << edges[i].src << " " << edges[i].dest << endl;
+			}
+		}
+	}
+};
+// Perform BFS on the graph starting from vertex `v`
+void BFS(Graph const& graph, int v, vector<bool>& discovered)
+{
+	// create a queue for doing BFS
+	queue<int> q;
+	// mark the source vertex as discovered
+	discovered[v] = true;
+	// enqueue source vertex
+	q.push(v);
+	// loop till queue is empty
+	while (!q.empty())
+	{
+		// dequeue front node and print it
+		v = q.front();
+		q.pop();
+		//cout << v << " ";
+		// do for every edge (v, u)
+		for (int u : graph.adjList[v])
+		{
+			if (!discovered[u])
+			{
+				// mark it as discovered and enqueue it
+				discovered[u] = true;
+				q.push(u);
+			}
+		}
+	}
+}
+int main()
+{
+
+	// vector of graph edges as per the above diagram
+
+	vector<Edge> edges =
+	{
+	{0,0,1},{1,1,1},{ 1, 2, 1}, {1, 3, 1}, {1, 4, 1}, {2, 5, 1}, {2, 6, 1}, {5, 9, 1},
+	{ 5, 10, 1 }, { 4, 7, 1 }, { 4, 8, 1 }, { 7, 11, 1 }, { 7, 12, 1 },{13,13,1},{14,14,1}
+	// vertex 0, 13, and 14 are single nodes
+	};
+	// total number of nodes in the graph (labelled from 0 to 14)
+	int n = 15;
+
+	random_device rd;
+	uniform_int_distribution<>ud(1, 100);
+
+	int x;
+	for (int i = 0; i < n; i++) {
+		x = ud(rd);
+		edges[i].value = x;
+	}
+
+	// build a graph from the given edges
+	Graph graph(edges, n);
+	// to keep track of whether a vertex is discovered or not
+	vector<bool> discovered(n, false);
+	// Perform BFS traversal from all undiscovered nodes to
+	// cover all connected components of a graph
+	for (int i = 0; i < n; i++)
+	{
+		if (discovered[i] == false)
+		{
+			// start BFS traversal from vertex `i`
+			BFS(graph, i, discovered);
+		}
+	}
+	cout << endl;
+	system("pause");
+	return 0;
+}
---
+++ DSF.cpp
@@ -0,0 +1,85 @@
+﻿//#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <random>
+
+using namespace std;
+
+class Graph
+{
+	int numVertices;
+	list<int>* adjLists;
+	bool* visited;
+
+public:
+	Graph(int V);
+	void addEdge(int src, int dest);
+	void DFS(int vertex);
+	void task();
+};
+
+// Graph initialization
+Graph::Graph(int vertices)
+{
+	numVertices = vertices;
+	adjLists = new list<int>[vertices];
+	visited = new bool[vertices];
+}
+
+// Adding edges
+void Graph::addEdge(int src, int dest)
+{
+	adjLists[src].push_front(dest);
+}
+
+// Algorithm
+void Graph::DFS(int vertex)
+{
+	visited[vertex] = true;
+	list<int> adjList = adjLists[vertex];
+	cout << vertex << " ";
+	list<int>::iterator i;
+	for (i = adjList.begin(); i != adjList.end(); ++i)
+		if (!visited[*i])
+			DFS(*i);
+}
+
+// Creating random values for each edge and find the highest value
+void Graph::task() {
+	int value = 0;
+	int maxValue = 0;
+	int maxVertex = 0;
+	random_device rd2;
+	uniform_int_distribution<> ud2(0, 100);
+	for (int vertex = 0; vertex < numVertices; vertex++) {
+		value = ud2(rd2);
+		cout << "Edge no. " << vertex << " has value: " << value << endl;
+		if (value > maxValue) {
+			maxVertex = vertex;
+			maxValue = value;
+		}
+	}
+	cout << "Max value : " << maxValue << " edge no. " << maxVertex << endl;
+}
+
+int main()
+{
+	random_device rd;
+	uniform_int_distribution<> ud(0, 3);
+	Graph g(4);
+	g.addEdge(0, 1);
+	g.addEdge(0, 2);
+	g.addEdge(1, 2);
+	g.addEdge(2, 3);
+	g.DFS(0);
+	cout << endl;
+	g.task();
+
+
+	system("pause");
+	return 0;
+}
